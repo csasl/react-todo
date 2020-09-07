c2ae08f4758f4e466c5c37c9092a47c6706d400b
csasl <azra_lalji@hotmail.com>
2020-09-06 19:38:34 -0700

    Updates

---
+++ precache-manifest.1b2ea8d8e90e2dc726ca5299592bf0c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4ac299ee163d607752919c581a7fa9",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "59f6f49da8078f6e91a1",
+    "url": "/react-todo/static/css/main.c9a989c9.chunk.css"
+  },
+  {
+    "revision": "3aad5de89f883c804185",
+    "url": "/react-todo/static/js/2.9858ddcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.9858ddcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f6f49da8078f6e91a1",
+    "url": "/react-todo/static/js/main.97fe482a.chunk.js"
+  },
+  {
+    "revision": "ce701484895442b2eb85",
+    "url": "/react-todo/static/js/runtime-main.e4e550a5.js"
+  }
+]);
